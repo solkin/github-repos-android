f816209e0ec7e9fa093a116f1a80b43bfa45fbd7
Igor Solkin <inbox@tomclaw.com>
2018-03-15 23:29:08 +0300

    Remove fixed access token; add screenshots for readme

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # GitHub Repos
 GitHub user repositories demo implementation
+
+![Screenshot](art/screen-1.png "Repositories list")
+![Screenshot](art/screen-2.png "Action menu")
--- app/src/main/java/com/tomclaw/githubrepos/core/Config.java
+++ app/src/main/java/com/tomclaw/githubrepos/core/Config.java
@@ -8,7 +8,7 @@ public class Config {
     public static final String API_HOST = "https://api.github.com/";
     public static final String USER = "solkin";
     public static final int REPOS_PER_PAGE = 10;
-    public static final String ACCESS_TOKEN = "142f7a43449c89beae3f3a96cc47de7f33ce1c08";
+    public static final String ACCESS_TOKEN = "";
 
     public static final String LOG_TAG = "githubrepos";
 
